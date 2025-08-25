5758de78fda2b2bff8a0edbb5b9ecd5257536602
Priyanshu Kumar <66831084+priykumar@users.noreply.github.com>
2025-08-25 10:11:21 +0530

    Update insertion.go

--- insertion.go
+++ insertion.go
@@ -6,9 +6,14 @@ func insertionSort(a []int) []int {
 	for i := 1; i < n; i++ {
 		ele := a[i]
 		j := i - 1
+
+		// Shift elements greater than ele to the right
 		for j >= 0 && a[j] > ele {
-			a[j], a[j+1] = a[j+1], a[j]
+			a[j+1] = a[j]
+			j--
 		}
+
+		// Place ele at the correct position
 		a[j+1] = ele
 	}
 
